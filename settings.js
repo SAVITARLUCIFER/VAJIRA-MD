6a6f5be375e338a8455284f680ca86914fb115df
SAVITARLUCIFER <88197679+SAVITARLUCIFER@users.noreply.github.com>
2024-07-20 17:31:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,VAJIRA-MD=uQUBUCLY#24n6RLXsYDUA4d5H4JPtPpoleBhYC-9qf1bPcBFfqpc
+SESSION_ID: process.env.SESSION_ID || 'youre session id' VAJIRA-MD=uQUBUCLY#24n6RLXsYDUA4d5H4JPtPpoleBhYC-9qf1bPcBFfqpc
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
